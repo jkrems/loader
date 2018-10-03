ca96e4eab9a7a321f04b95699ff02a67a8348981
Jan Krems <jan.krems@groupon.com>
2018-10-03 13:54:09 -0700

    refactor: Add forward-looking code for url-to-path

--- examples/require-from-path.mjs
+++ examples/require-from-path.mjs
@@ -1,4 +1,6 @@
 import Module from 'module';
+// TODO: Use once this lands in a node 10 release
+// import { fileURLToPath as fromPath } from 'url';
 
 const req = Module.createRequireFromPath(new URL(import.meta.url).pathname);
 export default req('./cjs.js');
